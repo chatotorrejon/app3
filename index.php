e68edd0c430524be81d0c969c6128923c34e7ec1
alfredo <alfredodelope@gmail.com>
2024-04-09 14:23:58 +0200

    seguimiento

--- index.php
+++ index.php
@@ -19,10 +19,28 @@
     </head>
 
     <body>
-        <header>
-            <!-- place navbar here -->
-        </header>
-        <main></main>
+        <nav
+            class="navbar navbar-expand-xxl navbar-dark  bg-primary mb-4"
+        >
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">CLASE DE COKTELERIA</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="navbarCollapse"
+                        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                        <form action="" class="d-flex">
+                            <input type="search" class="form-control me-2" placeholder="Search" aria-label=
+                            "Search">
+                            <button class="btn btn-outline-success" type="submit">Buscador</button>
+                        </form>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+        
         <footer>
             <!-- place footer here -->
         </footer>
